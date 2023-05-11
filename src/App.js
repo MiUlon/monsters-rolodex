513d127048bbc29e6cbce1cd73fb1323d07a9a7b
Mindaugas Ulonas <mindegi15@gmail.com>
2023-05-11 21:02:08 +0300

    learning setState

--- src/App.js
+++ src/App.js
@@ -16,15 +16,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi, { this.state.name }!</p>
-          <button>Change Name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={ () => { this.setState({ name: 'MindaugasOnClick'} ) } }>Change Name</button>
         </header>
       </div>
     );
