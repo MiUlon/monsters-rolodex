f55b2259bcc99910b47b6aa12240e509605bbecb
Mindaugas Ulonas <mindegi15@gmail.com>
2023-05-11 20:50:11 +0300

    learning State

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'MindaugasState'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi, { this.state.name }!</p>
+          <button>Change Name</button>
           <a
             className="App-link"
             href="https://reactjs.org"
